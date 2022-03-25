d4a09e6246c84dbc46ea1f97fe592aba0b1ce407
Nedas <neduska991@gmail.com>
2022-03-25 17:04:09 +0000

    Push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="line1"></div>
+      <div className="container">
+        <h1>Testing if it works</h1>
+      </div>
+      {/* Top line */}
+      <div className="line2"></div>
+      {/* Left line horizontal */}
+      <div className="line3"></div>
+      {/* Left line vertical */}
+      <div className="line4"></div>
+      {/* Right line vertical */}
+      <div className="line5"></div>
+      {/* Right line horizontal */}
+      <div className="line6"></div>
+      {/* Bottom line */}
+      <div className="line7"></div>
+      {/* Bottom line horizontal */}
+      <div className="line8"></div>
+
+      <div className="footer">
+        <p>Made by <a target="_blank" href="https://www.github.com/neozxc">Nss</a></p>
+      </div>
     </div>
   );
 }
