5fe546a5899b62a0f995c6229b37af22fb48739c
Nedas <neduska991@gmail.com>
2022-03-30 17:50:32 +0100

    .

--- src/components/WhiteBlock/WhiteBlock.js
+++ src/components/WhiteBlock/WhiteBlock.js
@@ -10,17 +10,17 @@ export const WhiteBlock = () => {
     <div className='block'>
         <div>
             <img width={150} src={firstImage}/>
-            <Link to="/teamcomps"><h1>Team Comps</h1></Link>
+            <Link to="/teamcomps"><h1 className='th1'>Team Comps</h1></Link>
         </div>
 
         <div>
             <img width={150} src={secondImage}/>
-            <Link to="/ranks"><h1>Ranks</h1></Link>
+            <Link to="/ranks"><h1 className='rh1'>Ranks</h1></Link>
         </div>
 
         <div>
             <img width={150} src={thirdImage}/>
-            <Link to="/agents"><h1>Agents</h1></Link>
+            <Link to="/agents"><h1 className='ah1'>Agents</h1></Link>
         </div>
 
         {/* <div className="line1"></div> */}
