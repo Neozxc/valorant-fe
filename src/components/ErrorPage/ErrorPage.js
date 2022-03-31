66cd5c4861f02117d232b9d749fb7ba02dd58a47
Nedas <neduska991@gmail.com>
2022-03-31 13:52:10 +0100

    .

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './ErrorPage.css'
+import Breach from '../Images/breach.webp'
+import { Link } from 'react-router-dom'
 
 export const ErrorPage = () => {
   return (
-    <div className='errorpage'>
-        <h1>This page was not found.</h1>
+    <div>
+      <div className='errorpage'>
+        <h1>Page you're looking not found.</h1>
+      </div>
+      <div className='goback'>
+        <Link to="/"><h1>Go back</h1></Link>
+      </div>
+        <img className='breach' src={Breach} alt="" />
     </div>
   )
 }
--- src/components/WhiteBlock/WhiteBlock.js
+++ src/components/WhiteBlock/WhiteBlock.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './WhiteBlock.css'
-import firstImage from '../Images/cheer-up.png'
-import secondImage from '../Images/rank.png'
-import thirdImage from '../Images/searching.png'
+import firstImage from '../Images/standing_jett.webp'
+import secondImage from '../Images/standing_phoenix.webp'
+import thirdImage from '../Images/standing_sage.png'
 
 export const WhiteBlock = () => {
   return (
     <div className='block'>
         <div>
-            <img width={150} src={firstImage}/>
+            <img src={firstImage}/>
             <Link to="/teamcomps"><h1 className='th1'>Team Comps</h1></Link>
         </div>
 
         <div>
-            <img width={150} src={secondImage}/>
+            <img src={secondImage}/>
             <Link to="/ranks"><h1 className='rh1'>Ranks</h1></Link>
         </div>
 
         <div>
-            <img width={150} src={thirdImage}/>
+            <img src={thirdImage}/>
             <Link to="/agents"><h1 className='ah1'>Agents</h1></Link>
         </div>
 
